34336232fe0f254a52cc1657df92f3f65ed81b86
gjcharlton <garycharltonxx@gmail.com>
2015-08-25 01:05:06 -0400

    Start of Game UI Implementation. Trials Implementation.
    The player is now able to select the amount of trials they would like to
    play, ranging from 1-9.
    There are UI prompts for this selection process.
    The player can now also play through multiple rounds. At the moment, it
    is confusing as to when a new round has actually started.
    An indication of such will be necessary.
    Also, if the player gets to the end of the trial and hasn't pressed
    space, there will need to be a 'loss' prompt.

---
+++ Assets/Scripts/InGameUI.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InGameUI : MonoBehaviour {
+
+	public static int Trials; //Amount of trials set by player in UI menu
+	public static bool trialCountSelected; // Checks if the player has finished picking the amount of trials
+
+	public GameObject trialInputCanvas; 
+	public GameObject confirmationCanvas;
+
+	//All buttons that coincide with trial amount
+    public Button One;
+    public Button Two;
+    public Button Three;
+    public Button Four;
+    public Button Five;
+    public Button Six;
+    public Button Seven;
+    public Button Eight;
+    public Button Nine;
+
+    public Text confirmationText; // Text for the confirmation once the player has chosen the amount of trials
+
+	void Update()
+	{
+		//If the player has picked their amont of trials and then proceeds to press space, they will begin gameplay
+		if (confirmationCanvas.activeSelf == true && Input.GetKeyDown (KeyCode.Space)) 
+		{
+			confirmationCanvas.SetActive(false);
+			trialCountSelected = true;
+			Time.timeScale = 1;
+		}
+	}
+
+	//All of these functions are UI functions that will change the text of the confirmation window depending on which you press.
+	//Each will change the int "Trials" appropriately
+	//Each will also turn off the trial input canvas once a number icon has been pressed, thus turning on the confirmation panel
+	public void OnePressed()
+	{
+		Trials = 1;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the spacebar to continue!";
+		trialInputCanvas.SetActive (false);
+		confirmationCanvas.SetActive (true);
+	}
+	public void TwoPressed()
+	{
+		Trials = 2;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the spacebar to continue!";
+		trialInputCanvas.SetActive (false);
+		confirmationCanvas.SetActive (true);
+	}
+	public void ThreePressed()
+	{
+		Trials = 3;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the spacebar to continue!";
+		trialInputCanvas.SetActive (false);
+		confirmationCanvas.SetActive (true);
+	}
+	public void FourPressed()
+	{
+		Trials = 4;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the spacebar to continue!";
+		trialInputCanvas.SetActive (false);
+		confirmationCanvas.SetActive (true);
+	}
+	public void FivePressed()
+	{
+		Trials = 5;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the spacebar to continue!";
+		trialInputCanvas.SetActive (false);
+		confirmationCanvas.SetActive (true);
+	}
+	public void SixPressed()
+	{
+		Trials = 6;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the spacebar to continue!";
+		trialInputCanvas.SetActive (false);
+		confirmationCanvas.SetActive (true);
+	}
+	public void SevenPressed()
+	{
+		Trials = 7;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the spacebar to continue!";
+		trialInputCanvas.SetActive (false);
+		confirmationCanvas.SetActive (true);
+	}
+	public void EightPressed()
+	{
+		Trials = 8;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the spacebar to continue!";
+		trialInputCanvas.SetActive (false);
+		confirmationCanvas.SetActive (true);
+	}
+	public void NinePressed()
+	{
+		Trials = 9;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the spacebar to continue!";
+		trialInputCanvas.SetActive (false);
+		confirmationCanvas.SetActive (true);
+	}
+
+	
+}
--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -3,20 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class TargetController : MonoBehaviour {
-
-	public bool Visible = true;
-	public bool Invisible = false;
-
-	public Sprite TargetSprite, Football, soccerBall, golfBall, Bow, Basketball, Volleyball, Nothing ; 
-
-	public List<Sprite> sportIcons;
-
-	// Use this for initialization
-	void Start () 
-	{
 	
 
-		sportIcons.Add (TargetSprite);
+	public bool Visible = true; //Used to communicate that the object is completely visible
+	public bool Invisible = false; //Used to communicate that the object is completely invisible
+
+	public bool isSwapping = false;
+
+	public Sprite targetSprite; //This is the Icon the player will be matching
+	public Sprite Football, soccerBall, golfBall, Bow, Basketball, Volleyball, Nothing; //These are the icons that will be used interchangeable for matching purposes
+
+	public List<Sprite> sportIcons;//List of sports icons
+	public bool emptyList = false; 
+	public bool showTarget = true; //Used to determine when the target should be shown
+
+	void Awake()
+	{
+		//Make sure the game doesn't start playing while picking trials. This essentially pauses the game temporarily.
+		Time.timeScale = 0;
+
+		//Adds my icons to the list
 		sportIcons.Add (Football);
 		sportIcons.Add (soccerBall);
 		sportIcons.Add (golfBall);
@@ -25,57 +31,184 @@ public class TargetController : MonoBehaviour {
 		sportIcons.Add (Volleyball);
 		sportIcons.Add (Nothing);
 
-
-		this.gameObject.GetComponent<SpriteRenderer> ().color = new Color (1.0f, 1.0f, 1.0f, 1.0f);
+		//Sets the target icon to be equal to a random one of the elements in my list
+		targetSprite = sportIcons [Random.Range (0, sportIcons.Count - 1)];
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = targetSprite;
+		//Debug.Log (targetSprite.name);
 
 
 	}
-	
+	// Use this for initialization
+	void Start () 
+	{
+		this.gameObject.GetComponent<SpriteRenderer> ().color = new Color (1.0f, 1.0f, 1.0f, 0.0f);
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
-		if (this.gameObject.GetComponent<SpriteRenderer> ().color.a >= .99f) 
+		if(InGameUI.trialCountSelected == true && InGameUI.Trials > 0 && emptyList == false)
 		{
-			Debug.Log("Should be Visible");
-			Visible = true;
-			Invisible = false;
+
+			if (this.gameObject.GetComponent<SpriteRenderer> ().color.a >= .99f) 
+			{
+				//Debug.Log("Should be Visible");
+				Visible = true;
+				Invisible = false;
+
+			if(showTarget)
+				{
+					showTarget = false;
+				}
+
+			} 
+
+			if (this.gameObject.GetComponent<SpriteRenderer> ().color.a <= .01f && isSwapping == false) 
+			{	
+				isSwapping = true;
+
+				if(sportIcons.Count == 0)
+					emptyList = true;
+
+				if(showTarget == true)
+				{
+					Visible = false;
+					Invisible = true;
+				}
+				else
+				StartCoroutine ("SpriteSwapCoroutine");
+				
+			} 
+			else if (this.gameObject.GetComponent<SpriteRenderer> ().color.a >= .01f && isSwapping == true)
+			{
+					isSwapping = false;
+			}
+		
+
+			if (!Invisible) 
+			{
+				this.gameObject.GetComponent<SpriteRenderer> ().color -= new Color (0, 0, 0, (.5f * Time.deltaTime));
+			}
+			if (!Visible) 
+			{
+				this.gameObject.GetComponent<SpriteRenderer> ().color += new Color (0, 0, 0, (.5f * Time.deltaTime));
+			}
+		
+
+		
+			if (Input.GetKeyDown (KeyCode.Space)) 
+			{
+				if (TargetChecker () == true) 
+				{
+					//run winner code
+					//Debug.Log ("You won!");
+					//	ResetTarget ();
+				} 
+				else 
+				{
+					//Debug.Log ("You Lost!");
+					//run loser code
+					//	ResetTarget ();
+				}
+			}
+		
+		
+
 		} 
-		else if (this.gameObject.GetComponent<SpriteRenderer> ().color.a <= .01f) 
-		{	
-			StartCoroutine("SpriteSwapCoroutine");
-		}
-
-		if (!Invisible) 
+		else if(emptyList == true)//If the list is empty, the amount of trials left is deducted and the game is reset
 		{
-			this.gameObject.GetComponent<SpriteRenderer> ().color -= new Color (0, 0, 0, (.5f * Time.deltaTime));
+			InGameUI.Trials--;
+			ResetTarget();
+			emptyList = false;
+			StartCoroutine ("SpriteSwapCoroutine");
 		}
-
-		if (!Visible) 
+		else 
 		{
-			this.gameObject.GetComponent<SpriteRenderer> ().color += new Color (0, 0, 0, (.5f * Time.deltaTime));
-		}
 
+			//Debug.Log ("You Lost Because you ran out of items!!");
+			//This is where I will run in some canvas stuff and make the player see that 
+			//they had lost and will need to select whether they would liek to try again
+
+			//Also, they will be able to press space in order to continue their trials.
+		}
 
 
 	}
 
+	//Used to allow for a pause between icon transitions
 	IEnumerator SpriteSwapCoroutine()
 	{
-		SpriteSwap ();
+
 		yield return new WaitForSeconds (1.0f);
+
+		SpriteSwap ();
 		Visible = false;
 		Invisible = true;
 
 	}
 
+	//Swaps the current sprite with another random element in the list of sprites that are remaining
 	void SpriteSwap()
 	{
 		this.gameObject.GetComponent<SpriteRenderer> ().sprite = sportIcons [Random.Range (0, sportIcons.Count)];
+		sportIcons.Remove(this.gameObject.GetComponent<SpriteRenderer> ().sprite);
+	}
+
+	void ShowTarget(Sprite Target)
+	{
+
+	}
+
+	//If the player still has trials left, the list will be reset, and a new target will be chosen
+	void ResetTarget()
+	{
+
+
+		sportIcons.Add (Football);
+		sportIcons.Add (soccerBall);
+		sportIcons.Add (golfBall);
+		sportIcons.Add (Bow);
+		sportIcons.Add (Basketball);
+		sportIcons.Add (Volleyball);
+		sportIcons.Add (Nothing);
+
+		targetSprite = sportIcons [Random.Range (0, sportIcons.Count - 1)];
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = targetSprite;
+		//Debug.Log (targetSprite.name);
+
+	}
+
+	//float ScoreAlgorithm()
+	//{
+
+	//}
+
+	//If the player hits space, this function will compare names of the target's sprite and the current sprite assigned to the game object
+	//and will return true or false accordingly
+	bool TargetChecker()
+	{
+		if (targetSprite.name == this.gameObject.GetComponent<SpriteRenderer> ().sprite.name) 
+		{
+			return true;
+		} 
+		else
+			return false;
+
 	}
 
 
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
