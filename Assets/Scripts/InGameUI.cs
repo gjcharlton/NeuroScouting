bbde106875d85878b2e4f12050c249f129525d5d
gjcharlton <garycharltonxx@gmail.com>
2015-08-29 00:17:08 -0400

    Finished
    Fixed major bug that was stopping the game from playing properly. Some
    logic was off. The coroutine was running more than it should have been.
    Game now has messages that respond to an incorrect press as well as if
    the amount of icons runs out.
    Trials will finish properly
    Transitions happen in a uniform fashion, always.

--- Assets/Scripts/InGameUI.cs
+++ Assets/Scripts/InGameUI.cs
@@ -4,108 +4,185 @@ using System.Collections;
 
 public class InGameUI : MonoBehaviour {
 
-	public static int Trials; //Amount of trials set by player in UI menu
-	public static bool trialCountSelected; // Checks if the player has finished picking the amount of trials
-	public static bool isTargetShowing;
-	public bool scoreGrabbed = false;
+	public static int Trials = 0; //Amount of trials set by player in UI menu
+	public static bool trialCountSelected = false; // Checks if the player has finished picking the amount of trials
+	public static bool isTargetShowing = false; //Is the target to be matched showing?
 
-	public GameObject trialInputCanvas, confirmationCanvas, winnerCanvas, targetIndicatorCanvas, endGameCanvas;
+	//Canvas containers
+	public GameObject trialInputCanvas, confirmationCanvas, winnerCanvas, targetIndicatorCanvas, endGameCanvas, loserCanvas, endTrialCanvas;
 
-	//All buttons that coincide with trial selection amount
-	public Button One, Two, Three, Four, Five, Six, Seven, Eight, Nine;
 
-	public float totalScore = 0.0f;
-	public int trialsPlayed = 0;
-
-    public Text confirmationText; // Text for the confirmation once the player has chosen the amount of trials
+	//varying texts for the different canvases
+	public Text confirmationText; 
 	public Text winnerText;
 	public Text endGameText;
 
-	string tempString;
+	//All buttons that coincide with trial selection amount
+	public Button One, Two, Three, Four, Five, Six, Seven, Eight, Nine;
+	
+	bool scoreGrabbed = false;
+	bool updateResults = false;
 
-	public bool updateResults = false;
+	float totalScore = 0.0f;
+
+	int trialsPlayed = 0;
+
+	string tempEndGameString = "";
+	string tempConfirmationString = "";
+
+
 
 
 	void Update()
 	{
+
+		if (isTargetShowing && trialCountSelected == true) 
+		{
+
+			targetIndicatorCanvas.SetActive(true);
+
+		}
+		else
+		{
+
+			targetIndicatorCanvas.SetActive(false);
+
+		}
+
+
 
 		if (TargetController.Winner == true) 
 		{
 			if(scoreGrabbed == false)
 			{
 				winnerCanvas.SetActive(true);
-				winnerText.text = "You made a correct match!\n Your score for that match is: " + Mathf.Ceil(TargetController.Score) + "\n Press return/enter to continue!";
+				winnerText.text = "You made a correct match!\n Your score for that match is: " + Mathf.Ceil(TargetController.Score) + "\n Press 'C' to continue!";
 				totalScore += Mathf.Ceil(TargetController.Score);
 				scoreGrabbed = true;
 				TargetController.Score = 100;
 			}
 
+			if (winnerCanvas.activeSelf == true) 
+			{
+				Time.timeScale = 0; //Stop actions from occuring
+				
+				if(Input.GetKeyDown (KeyCode.C)) //If the player is prepared for the next round
+				{
+					TargetController.Winner = false;
+					winnerCanvas.SetActive(false);
+					scoreGrabbed = false;
+					trialsPlayed++;
+					
+					if(Trials == 0)
+					{
+						updateResults = true;
+						endGameCanvas.SetActive(true);
+						
+					}
+					else
+					{
+						Time.timeScale = 1;
+					}
+					
+				}
+				
+			}	
+
 		}
 
-		if (isTargetShowing && trialCountSelected == true) 
+		if (TargetController.Loser == true) 
 		{
-			targetIndicatorCanvas.SetActive(true);
-		}
-		else
-		{
-			targetIndicatorCanvas.SetActive(false);
+			loserCanvas.SetActive(true);
+			
+			if (loserCanvas.activeSelf == true) 
+			{
+				Time.timeScale = 0; //Stop actions from occuring
+				
+				if(Input.GetKeyDown (KeyCode.C)) //If the player is prepared for the next round
+				{
+					TargetController.Loser = false;
+					loserCanvas.SetActive(false);
+					trialsPlayed++;
+					
+					if(Trials == 0)
+					{
+						updateResults = true;
+						endGameCanvas.SetActive(true);
+					}
+					else
+					{
+						Time.timeScale = 1;
+					}
+					
+				}
+				
+			}	
+			
 		}
 
+		if(TargetController.endTrial == true)
+		{
+			endTrialCanvas.SetActive(true);
+
+			if (endTrialCanvas.activeSelf == true) 
+			{
+				Time.timeScale = 0; //Stop actions from occuring
+				
+				if(Input.GetKeyDown (KeyCode.C)) //If the player is prepared for the next round
+				{
+					TargetController.endTrial = false;
+					endTrialCanvas.SetActive(false);
+					trialsPlayed++;
+					
+					if(Trials == 0)
+					{
+						updateResults = true;
+						endGameCanvas.SetActive(true);
+					}
+					else
+					{
+						Time.timeScale = 1;
+					}
+					
+				}
+				
+			}	
+
+		}
 
 		//If the player has picked their amont of trials and then proceeds to press space, they will begin gameplay
-		if (confirmationCanvas.activeSelf == true && Input.GetKeyDown (KeyCode.Return)) 
+		if (confirmationCanvas.activeSelf == true && Input.GetKeyDown (KeyCode.C)) 
 		{
 			confirmationCanvas.SetActive(false);
 			trialCountSelected = true;
+			confirmationText.text = tempConfirmationString;
 			Time.timeScale = 1;
+
 		}
-		if (winnerCanvas.activeSelf == true) 
-		{
-			Time.timeScale = 0; //Stop actions from occuring
 
-			if(Input.GetKeyDown (KeyCode.Return)) //If the player is prepared for the next round
-			{
-				TargetController.Winner = false;
-				winnerCanvas.SetActive(false);
-				scoreGrabbed = false;
-				trialsPlayed++;
-
-				if(Trials == 0)
-				{
-					updateResults = true;
-					endGameCanvas.SetActive(true);
-
-				}
-				else
-				{
-					Time.timeScale = 1;
-				}
-
-			}
-
-		}	
 
 		if(endGameCanvas.activeSelf == true) // If the player has no more trials after hitting return, they get sent back to the trials menu
 		{
 			if(updateResults == true)
 			{
-				tempString = endGameText.text;
+				tempEndGameString = endGameText.text;
 				endGameText.text = endGameText.text + (totalScore/trialsPlayed);
 				updateResults = false;
 			}	
 
-			if(Input.GetKeyDown (KeyCode.Space)) //If the player is prepared for the next round
+			if(Input.GetKeyDown (KeyCode.X)) //If the player is prepared for the next round
 			{
 				trialsPlayed = 0;
 				totalScore = 0;
 				endGameCanvas.SetActive(false);
-				endGameText.text = tempString;
+				endGameText.text = tempEndGameString;
 				trialCountSelected = false;
 				trialInputCanvas.SetActive(true);
 			}
 
 		}
 	
+
 
 	
 
@@ -117,63 +194,72 @@ public class InGameUI : MonoBehaviour {
 	public void OnePressed()
 	{
 		Trials = 1;
-		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the return to continue!";
+		tempConfirmationString = confirmationText.text;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n\n" + confirmationText.text;
 		trialInputCanvas.SetActive (false);
 		confirmationCanvas.SetActive (true);
 	}
 	public void TwoPressed()
 	{
 		Trials = 2;
-		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the return to continue!";
+		tempConfirmationString = confirmationText.text;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n\n" + confirmationText.text;
 		trialInputCanvas.SetActive (false);
 		confirmationCanvas.SetActive (true);
 	}
 	public void ThreePressed()
 	{
 		Trials = 3;
-		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the return to continue!";
+		tempConfirmationString = confirmationText.text;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n\n" + confirmationText.text;
 		trialInputCanvas.SetActive (false);
 		confirmationCanvas.SetActive (true);
 	}
 	public void FourPressed()
 	{
 		Trials = 4;
-		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the return to continue!";
+		tempConfirmationString = confirmationText.text;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n\n" + confirmationText.text;
 		trialInputCanvas.SetActive (false);
 		confirmationCanvas.SetActive (true);
 	}
 	public void FivePressed()
 	{
 		Trials = 5;
-		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the return to continue!";
+		tempConfirmationString = confirmationText.text;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n\n" + confirmationText.text;
 		trialInputCanvas.SetActive (false);
 		confirmationCanvas.SetActive (true);
 	}
 	public void SixPressed()
 	{
 		Trials = 6;
-		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the return to continue!";
+		tempConfirmationString = confirmationText.text;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n\n" + confirmationText.text;
 		trialInputCanvas.SetActive (false);
 		confirmationCanvas.SetActive (true);
 	}
 	public void SevenPressed()
 	{
 		Trials = 7;
-		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the return to continue!";
+		tempConfirmationString = confirmationText.text;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n\n" + confirmationText.text;
 		trialInputCanvas.SetActive (false);
 		confirmationCanvas.SetActive (true);
 	}
 	public void EightPressed()
 	{
 		Trials = 8;
-		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the return to continue!";
+		tempConfirmationString = confirmationText.text;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n\n" + confirmationText.text;
 		trialInputCanvas.SetActive (false);
 		confirmationCanvas.SetActive (true);
 	}
 	public void NinePressed()
 	{
 		Trials = 9;
-		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n Press the return to continue!";
+		tempConfirmationString = confirmationText.text;
+		confirmationText.text = "You have chosen " + Trials + " trial(s) to play.\n\n" + confirmationText.text;
 		trialInputCanvas.SetActive (false);
 		confirmationCanvas.SetActive (true);
 	}
--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -4,23 +4,27 @@ using System.Collections.Generic;
 
 public class TargetController : MonoBehaviour {
 	
-	public static float Score;//Players score
-	public static bool Winner;
+	public static float Score = 100;//Players score
+	public static bool Winner = false; //Determines if the player has won a round by making a match. This is also used in InGameUI.cs
+	public static bool Loser = false; //Determines if the player has lost a round. This is also used in InGameUI.cs
+	public static bool endTrial = false;
+
+	public List<Sprite> sportIcons;//List of sports icons
+
+	public Sprite targetSprite; //This is the Icon the player will be matching
+	public Sprite Nothing; //This is what the target sprite will swap with if it is determined to be invisible this round
+	public Sprite Football, soccerBall, golfBall, Bow, Basketball, Volleyball; //These are the icons that will be used interchangeably for matching purposes
 
 	public bool Visible = true; //Used to communicate that the object is completely visible
 	public bool Invisible = false; //Used to communicate that the object is completely invisible
+	public bool hideTarget = false; //Determines whether this trial will hide the target
+	public bool isSwapping = false; //Determines whether or not the object has hit the point which it is swapping out sprites
+	public bool emptyList = false; //Determines if the list of sprites has become empty
+	public bool showTarget = true; //Used to make sure that a coroutine isn't called to swap the target sprite immediately since the object starts with an alpha of 0
+	public bool stopRoutine = false; //Used to stop coroutine from being called more than it should
+	public bool lastIcon = false;	
 
-	bool isSwapping = false;
-
-	public Sprite targetSprite; //This is the Icon the player will be matching
-	public Sprite Football, soccerBall, golfBall, Bow, Basketball, Volleyball, Nothing; //These are the icons that will be used interchangeable for matching purposes
-
-	public List<Sprite> sportIcons;//List of sports icons
-	public bool emptyList = false; 
-	public bool showTarget = true; //Used to determine when the target should be shown
-
-
-	public int swapCounter = 0;
+	public int swapCounter = 0; //Keeps track of how many times a swpa has been made in a trial
 
 	void Awake()
 	{
@@ -40,162 +44,224 @@ public class TargetController : MonoBehaviour {
 		targetSprite = sportIcons [Random.Range (0, sportIcons.Count - 1)];
 		this.gameObject.GetComponent<SpriteRenderer> ().sprite = targetSprite;
 
+		//Alogrithm to randomly determine whether or not the target should be invisible this round.
+		if(Random.Range(0,6).Equals(0))
+		{
+			hideTarget = true;
+			sportIcons.Remove (this.gameObject.GetComponent<SpriteRenderer> ().sprite);
+		}
+		else 
+		{
+			hideTarget = false;
+			sportIcons.Remove(Nothing);
+		}
+	
 
 	}
+
 	// Use this for initialization
 	void Start () 
 	{
+		//Starts the Target off with an alpha of 0 and lets the game know that it is supposed to be invisible, with an alpha of zero.
 		showTarget = true;
+		Visible = false;
+		Invisible = true;
 		this.gameObject.GetComponent<SpriteRenderer> ().color = new Color (1.0f, 1.0f, 1.0f, 0.0f);
+
 		Score = 100.0f;
+
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
 	
+
+		//Ensures that the player has selected a number of trials, tha they still have trials and that the list of icons isn't empty
 		if(InGameUI.trialCountSelected == true && InGameUI.Trials > 0 && emptyList == false)
 		{
+		
+
+			//Used for showing UI showing the player that the target is currently up
 			if(swapCounter == 0)
 			{
+
 				InGameUI.isTargetShowing = true;
+
 			}
 			else
 			{
+
 				InGameUI.isTargetShowing = false;
+
 			}
 
-			if (this.gameObject.GetComponent<SpriteRenderer> ().color.a >= .99f) 
-			{
-			
-				Visible = true;
-				Invisible = false;
 
-			} 
 
-			if (this.gameObject.GetComponent<SpriteRenderer> ().color.a <= .01f && isSwapping == false) 
+			if (this.gameObject.GetComponent<SpriteRenderer> ().color.a <= .1f && isSwapping == false) 
 			{		
-				print("herp");
+				//this.gameObject.GetComponent<SpriteRenderer> ().color = new Color (1.0f, 1.0f, 1.0f, 0.0f);
 				isSwapping = true;
-
+			
+				//if this is the last object and it gets to a low alpha, this is what will help transition us to a loss screen.
 				if(sportIcons.Count == 0)
-					emptyList = true;
+				{
+					Invoke("EmptyList", 2.5f);
 
-				if(showTarget == true)
-				{
-					if(swapCounter == 0 )
-					{
-						showTarget = false;
-					}
-					Visible = false;
-					Invisible = true;
 				}
-				else
+
+				//used to ensure that the current sprite isn't the target and that the routine only gets run once
+				if(showTarget == false && stopRoutine == false)// !stopRoutine)
 				{
-					print("Herp");
+
+					stopRoutine = true;
 					StartCoroutine ("SpriteSwapCoroutine");
+
 				}
+			
 			} 
-			else if (this.gameObject.GetComponent<SpriteRenderer> ().color.a >= .2f);
+			else if (this.gameObject.GetComponent<SpriteRenderer> ().color.a > .1f); //This entails that it is becoming visible again
 			{
+				//Subtract from the score because once the player can see it, and do not press space, they lose some of their score
+				Score -= (10*Time.deltaTime);
 
+				//Once the pbject hits its "max" visibility
+				if (this.gameObject.GetComponent<SpriteRenderer> ().color.a >= .99f) 
+				{
 
-				Score -= (15*Time.deltaTime);
+					//tell the game that it is in fact 100% visible
+					Visible = true;
+					Invisible = false;
 
+					//used the first time an icon comes to full alpha. Will only get used on the target and this ensures that the coroutine will run now
+					if(showTarget == true)
+					{
+
+						showTarget = false;
+
+					}
+
+					stopRoutine = false;
+					
+				} 
+				//showTarget = false;
+			
+				//if the alpha is increasing, then the swapping is over
 				if(isSwapping == true)
 				{
+
 					isSwapping = false;
+
 				}
 
 			}
-		
 
+			// if the sprite is registered as visible, start to make it invisible. Also ensures that it cannot go below a value of 0
 			if (!Invisible) 
 			{
-				this.gameObject.GetComponent<SpriteRenderer> ().color -= new Color (0, 0, 0, (.5f * Time.deltaTime));
+
+				if(this.gameObject.GetComponent<SpriteRenderer> ().color.a >= 0)
+				{
+
+					this.gameObject.GetComponent<SpriteRenderer> ().color -= new Color (0, 0, 0, (1 * Time.deltaTime));
+
+				}
+
 			}
+			// if the sprite is registered as invisible, start to make it visible. 
 			if (!Visible) 
 			{
-				this.gameObject.GetComponent<SpriteRenderer> ().color += new Color (0, 0, 0, (.5f * Time.deltaTime));
-			}
-		
 
-		
+				this.gameObject.GetComponent<SpriteRenderer> ().color += new Color (0, 0, 0, (1 * Time.deltaTime));
+
+			}
+
+			//once the player hits space to make a match
 			if (Input.GetKeyDown (KeyCode.Space)) 
 			{
+
+				//check if the target matches the current sprite, if so, take away from how many trials are left, reset the list and other variable, and show the winner popup
+				if(hideTarget == true && this.gameObject.GetComponent<SpriteRenderer>().sprite == Nothing )
+				{
+					CancelInvoke();
+					InGameUI.Trials--;
+					ResetTarget();
+					Winner = true;
+
+				}
+				//This is used in a case that the target sprite doesn't get shown during the trial and the player manages to make the match by realizing this
 				if (TargetChecker () == true && Winner == false) 
 				{
+
+					CancelInvoke();
 					InGameUI.Trials--;
 					ResetTarget();
 					Winner = true;
 
 				} 
-				else 
+				else if(TargetChecker () == false && Loser == false && swapCounter > 0) //If the player messess up, the loser window pops up. A trial is also subtracted
 				{
-					//Winner = false;
-					//InGameUI.Trials--;
-					//ResetTarget();
-					//emptyList = false;
-					//StartCoroutine ("SpriteSwapCoroutine");
+
+					CancelInvoke();
+					InGameUI.Trials--;
+					ResetTarget();
+					Loser = true;
+
 				}
+
 			}
-		
-		
 
 		} 
 		else if(emptyList == true && swapCounter == 6)//If the list is empty, the amount of trials left is deducted and the game is reset
 		{
+
 			InGameUI.Trials--;
 			ResetTarget();
-			StartCoroutine ("SpriteSwapCoroutine");
+			endTrial = true;
+
 		}
-		else 
-		{
-
-
-			//This is where I will run in some canvas stuff and make the player see that 
-			//they had lost and will need to select whether they would liek to try again
-
-			//Also, they will be able to press space in order to continue their trials.
-		}
-
-
 	}
 
 	//Used to allow for a pause between icon transitions
 	IEnumerator SpriteSwapCoroutine()
 	{
+
 		yield return new WaitForSeconds (1.0f);
-		swapCounter++;
-		Score = 100;
-		SpriteSwap ();
+
+		SpriteSwap();
+
 		Visible = false;
 		Invisible = true;
 
+
+		swapCounter++;
+		Score = 100;
+	
 	}
 
 	//Swaps the current sprite with another random element in the list of sprites that are remaining
 	void SpriteSwap()
 	{
+		//if(hideTarget == false)
+
 		this.gameObject.GetComponent<SpriteRenderer> ().sprite = sportIcons [Random.Range (0, sportIcons.Count)];
 		sportIcons.Remove(this.gameObject.GetComponent<SpriteRenderer> ().sprite);
-	}
-
-	void ShowTarget(Sprite Target)
-	{
-
 	}
 
 	//If the player still has trials left, the list will be reset, and a new target will be chosen
 	void ResetTarget()
 	{
-		swapCounter = 0;
-		sportIcons.Clear ();
-		showTarget = true;
 
+		Visible = false;
+		Invisible = true;
+
+		swapCounter = 0;
+
+		sportIcons.Clear ();
+
+		showTarget = true;
 		 
 		this.gameObject.GetComponent<SpriteRenderer> ().color = new Color (1.0f, 1.0f, 1.0f, 0.0f);
-
 
 		sportIcons.Add (Football);
 		sportIcons.Add (soccerBall);
@@ -205,18 +271,23 @@ public class TargetController : MonoBehaviour {
 		sportIcons.Add (Volleyball);
 		sportIcons.Add (Nothing);
 
-
 		emptyList = false; 
 
 		targetSprite = sportIcons [Random.Range (0, sportIcons.Count - 1)];
 		this.gameObject.GetComponent<SpriteRenderer> ().sprite = targetSprite;
+
+		if(Random.Range(0,4).Equals(1))
+		{
+			hideTarget = true;
+			sportIcons.Remove (this.gameObject.GetComponent<SpriteRenderer> ().sprite);
+		}
+		else 
+		{
+			hideTarget = false;
+			sportIcons.Remove(Nothing);
+		}
+
 	}
-
-
-	//float ScoreAlgorithm()
-	//{
-
-	//}
 
 	//If the player hits space, this function will compare names of the target's sprite and the current sprite assigned to the game object
 	//and will return true or false accordingly
@@ -229,6 +300,11 @@ public class TargetController : MonoBehaviour {
 		else
 			return false;
 
+	}
+
+	void EmptyList()
+	{
+		emptyList = true;
 	}
 
 
